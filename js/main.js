401c2b4d4edbbfe7b507ff6dc3d469c434a7c333
Nico Bobb <nicobobb@gmail.com>
2024-03-05 18:59:30 -0300

    guarda en objetos a los usuarios

--- js/main.js
+++ js/main.js
@@ -1,1 +1,24 @@
-console.log("Cargando js");
+const form = document.getElementById("form");
+const nameInput = document.getElementById("name");
+const addressInput = document.getElementById("address");
+const emailInput = document.getElementById("email");
+const insuranceInput = document.getElementById("insurance");
+
+let userObject = [];
+
+const AddUserObject = (newUser) => {
+    userObject.push(newUser);
+    console.log("Todos los usuarios:", userObject);
+};
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const newUser = {
+        name: nameInput.value,
+        address: addressInput.value,
+        email: emailInput.value,
+        insurance: insuranceInput.value,
+    };
+    console.log(newUser);
+    AddUserObject(newUser);
+});
